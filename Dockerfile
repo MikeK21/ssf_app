7f5df923d3ca44d68a3ca158ccebd9d4aa65764f
Mike Kiriakou <mikexkiriakou@gmail.com>
2021-12-20 10:38:18 -0500

    working flask application commit

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3.6
 MAINTAINER Mike Kiriakou "whomikek@gmail.com"
+ENV STOCKSYMBOL "MSFT"
+ENV NDAYS "3"
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 from flask import Flask
 import os
+import json
+import scrape_mon
+import urllib.request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Flask inside Docker!!"
 
+def hello():
+    #return "HELLO"
+    try:
+    #    with open("stock.txt","r") as stock_file:
+    #	    resultSet = stock_file.readlines()
+        resultSet = scrape_mon.getEnvRequest()
+    except:
+        print("Could not contact scrape_mon")
+#    file = open(r'./scrape_mon.py', 'r').read()
+#    return exec(file)
+#    return json.dumps(resultSet)
+#     return hello_func()
+    return resultSet
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
